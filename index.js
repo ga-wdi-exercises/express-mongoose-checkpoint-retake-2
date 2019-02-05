5422ea958cd34b328b936b0d71fcfba806622c40
Roger Campbell II <rc1336@gmail.com>
2019-02-05 14:23:39 -0500

    Finished the owl..

--- index.js
+++ index.js
@@ -2,24 +2,41 @@
 // in order to use partials you can use hbs to register the partials directory
 // setting a partial directory was covered in a previous exercise
 // partials have been provided in views/partials/
+const express = require('express')
+const app = express()
+// const cors = require('cors')
+const Quest = require('./models/Quest')
+
+app.set('view engine', 'hbs')
+
+
 
 
 //the below routes can be used or changed as you see fit
+
+// GET '/' which should redirect to `/quests'
 app.get("/", (req, res) => {
- res.send('this should redirect to the /quests route')
+ res.redirect('/quests')
 })
 
-app.get("/quests", (req, res) => {
-  res.send('this should show all the quests')
+// GET '/quests' which render view with a list of notes
+app.get('/quests', (req, res) => {
+  Quest.find({})
+  .then(quest => {
+    res.render("index", { quest })
+  })
 })
 
+// GET '/quest/:someParameter' where :someParameter can be used to identify or grab a single Quest from the database.
 app.get("/quest/:id", (req, res) => {
-  res.send('this should show a single quest')
+  Quest.findOne({_id: req.params.id})
+  .then(quest => {
+      res.render("show", { quest });
+    })
 })
 
 
-app.listen(3000, () => console.log('app is running'))
+app.listen(3000, () => console.log('app is running Skyrim Rocks!'))
 
 // DO NOT REMOVE THIS LINE:
 module.exports = app
-
