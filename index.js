b3e2a2cd946df6efcff6454223ede251bc840aed
Evan Canoy <evan@Evans-MacBook-Pro.local>
2019-02-05 13:57:38 -0500

    retake of the retake

--- index.js
+++ index.js
@@ -2,19 +2,22 @@
 // in order to use partials you can use hbs to register the partials directory
 // setting a partial directory was covered in a previous exercise
 // partials have been provided in views/partials/
-
-
+const express = require('express')
+const app = express()
+app.set('view engine', 'hbs')
 //the below routes can be used or changed as you see fit
 app.get("/", (req, res) => {
- res.send('this should redirect to the /quests route')
+    res.redirect('./quests')
+
 })
 
 app.get("/quests", (req, res) => {
-  res.send('this should show all the quests')
+  res.render()
 })
 
 app.get("/quest/:id", (req, res) => {
-  res.send('this should show a single quest')
+    res.render('./show')
+
 })
 
 
--- models/Quest.js
+++ models/Quest.js
@@ -1,4 +1,10 @@
 const mongoose = require('../db/connection')
 
+const Quest = new mongoose.Schema({
+    title: String,
+    quest_giver: String,
+    rewards: [],
+    locations: []
+})
 
 module.exports = mongoose.model('Quest', Quest)
