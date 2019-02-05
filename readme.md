bff3a6a0c05c25fc20e2eac218e207fd5f65af3c
Jimmy Byess <james.byess@gmail.com>
2019-02-05 13:25:38 -0500

    checkpoint retake initial commit

---
+++ readme.md
@@ -0,0 +1,87 @@
+# Checkpoint: Express & Mongoose Retake
+
+You may use lesson plans for guidance during this checkpoint, such as the
+[Express MVC lesson plan](https://git.generalassemb.ly/ga-wdi-lessons/express-mongoose).
+
+## Set Up
+
+Fork and clone this repository. After cloning down **your fork**...
+
+```bash
+ $ cd express-mongoose-checkpoint-retake
+ $ npm install
+```
+
+To run the tests in the terminal, run `npm test`.
+
+- Every time you make a change and save your files, you will need to manually
+  re-run the tests.
+- When you start, it will report 3 tests are passing and 6 are failing
+  (sometimes it will say 4 out of 6). Your goal is to make as many pass as you
+  can in the hour provided. As you complete tests more will be revealed, there
+  are a total of 17 tests, you need to complete 11 tests to pass the checkpoint.
+
+- Read the output of the failing tests. It will tell you why the test failed.
+
+> Note: The test suite is located in `./test`. Do not touch any of the files in
+> this folder.
+
+## Instructions
+
+For this checkpoint, you need to build a simple application for tracking Quests.
+
+### Requirements
+
+1. There is already a `package.json` file with the dependencies necessary for
+   running the tests. You will have to manually install everything else required
+   to build your application server with Express, Mongoose, and Handlebars (use
+   `hbs` instead of `express-handlebars`).
+
+1. Require and initialize express in your `index.js` file.
+
+1. Create a new schema in your `models/Quest.js` file and store it in a variable
+   called `Quest`, with 4 fields. Each field is expected to have a specific data
+   type:
+
+- title: String
+- quest_giver: String
+- rewards: Array
+- locations: Array
+
+4. You should have 3 routes...
+
+- `GET '/'` which should redirect to `/quests'
+- `GET '/quests'` which render view with a list of notes
+- `GET '/quest/:someParameter'` where `:someParameter` can be used to identify
+  or grab a single `Quest` from the database.
+
+5. Configure your express application to use `hbs` as the view engine.
+
+6. Your application should have the following views...
+
+- A handlebars layout file named `views/layout.hbs`.
+- A `views/index.hbs` lists all the Quests, **showing the: `title` `quest_giver`
+  of each `Quest`**.
+- A show view (`views/show.hbs`) for a `Quest` that, **displays the `Quests`'s
+  `title`, `quest_giver`, `rewards` and `locations`**.
+
+**NOTES:**
+
+- We've provided the necessary configuration for the database connection, some
+  of the Model definition, and some of the basic express app boilerplate. Don't
+  delete any of this! Your task is to add to what is provided.
+- The general structure of the views has been provided, you can use this if you
+  like, but you don't have to, you only need the tests to pass.
+- The general structure of a seed is provided, this can help those that like to
+  see what they are developing or those that cannot run the tests. It will not
+  run as it is, it is missing something that has been explored in a previous
+  project.
+- If your tests time-out, don't exit, try hitting `ctrl-c`.
+- Do not modify the tests in `tests/index.js`!
+
+## Submission
+
+Fork this repository and clone your fork locally. Commit as you go. When you are
+finished, push to GitHub.
+
+**Wait until the end of the hour to make your Pull Request**
---
+++ test/index.js
@@ -0,0 +1,226 @@
+const chai = require("chai")
+const expect = require("chai").expect
+const http = require("chai-http")
+
+const package = require("../package.json")
+
+chai.use(http)
+
+describe("Setup -", () => {
+  beforeEach(done => {
+    done()
+  })
+
+  afterEach(done => {
+    done()
+  })
+
+  describe("package.json dependencies", () => {
+    it("should contain Express", done => {
+      expect(package.dependencies.express).not.to.be.undefined
+      done()
+    })
+
+    it("should contain Mongoose", done => {
+      expect(package.dependencies.mongoose).not.to.be.undefined
+      done()
+    })
+
+    it("should contain hbs", done => {
+      expect(package.dependencies.hbs).not.to.be.undefined
+      done()
+    })
+  })
+
+  describe("The project file structure ", () => {
+    it("should have a layout.hbs at ./views/layout.hbs", done => {
+      let importLayout = () => require("../views/layout.hbs")
+
+      // expect(importLayout).to.not.throw()
+      expect(importLayout).not.to.be.undefined
+      done()
+    })
+
+    it("should have an index.hbs at ./views/index.hbs", done => {
+      let importIndexView = () => require("../views/index.hbs")
+
+      // expect(importIndexView).to.not.throw()
+      expect(importIndexView).not.to.be.undefined
+      done()
+    })
+
+    it("should have a show.hbs at ./views/show.hbs", done => {
+      let importShowView = () => require("../views/show.hbs")
+
+      // expect(importShowView).to.not.throw()
+      expect(importShowView).not.to.be.undefined
+      done()
+    })
+
+    it("should have a Quest model inside ./models/Quest.js", done => {
+      let importModel = () => require("../models/Quest")
+      expect(importModel).to.not.throw()
+      expect(importModel).not.to.be.undefined
+      done()
+    })
+  })
+})
+
+describe("Model -", () => {
+  let Quest
+  let app
+
+  before(done => {
+    Quest = require("../models/Quest")
+    app = require("../index.js")
+    done()
+  })
+
+  beforeEach(done => {
+    Quest.deleteMany({}).then(() => {
+      done()
+    })
+  })
+
+  afterEach(done => {
+    Quest.deleteMany({}).then(() => {
+      done()
+    })
+  })
+
+  it("should have a Quest model", done => {
+    expect(Quest).not.to.be.undefined
+    done()
+  })
+
+  it("should have a title property on the model", done => {
+    expect(Quest.schema.obj).to.have.property("title")
+    done()
+  })
+
+  it("should have a quest_giver property on the model", done => {
+    expect(Quest.schema.obj).to.have.property("quest_giver")
+    done()
+  })
+
+  it("should have a rewards property on the model", done => {
+    expect(Quest.schema.obj).to.have.property("rewards")
+    done()
+  })
+
+  it("should have a locations property on the model", done => {
+    expect(Quest.schema.obj).to.have.property("locations")
+    done()
+  })
+
+  it("should be able to create a Quest", done => {
+    Quest.create({
+      "title": "Taking Care of Business",
+      "quest_giver": "Brynjolf",
+      "rewards": ["Thieves Guild Outfit", "Fence"],
+      "locations": ["Riften"]
+    }).then(quest => {
+      expect(quest).not.to.be.undefined
+      expect(quest).to.be.an("object")
+
+      expect(quest.quest_giver).not.to.be.undefined
+      expect(quest.title).not.to.be.undefined
+      expect(quest.rewards).not.to.be.undefined
+      expect(quest.locations).not.to.be.undefined
+
+
+      expect(quest.title).to.be.a("string")
+      expect(quest.quest_giver).to.be.a("string")
+      expect(quest.rewards).to.be.a("array")
+      expect(quest.locations).to.be.a("array")
+      done()
+    })
+  })
+})
+
+describe("Routes -", () => {
+  this.timeout = 1000
+
+  let Quest
+  let app
+
+  before(done => {
+    Quest = require("../models/Quest")
+    app = require("../index.js")
+    done()
+  })
+
+  beforeEach(done => {
+    Quest.deleteMany({}).then(() => {
+      Quest.create({
+        "title": "House of Horrors",
+        "quest_giver": "Vigilant Tyranus",
+        "rewards": ["Abandoned House (Markarth)"],
+        "locations": ["Abandoned House"]
+      }).then(() => done())
+    })
+  })
+
+  afterEach(done => {
+    Quest.deleteMany({}).then(() => done())
+  })
+
+  describe("A GET request for the homepage ('/')", () => {
+    it("should return a successful response", done => {
+      chai
+        .request(app)
+        .get("/")
+        .end((err, res) => {
+          expect(res).to.have.status(200)
+          expect(res).to.be.html
+          done()
+        })
+    })
+
+    it("should include a list of Quests with quest_givers, titles, rewards and locations displayed", done => {
+      Quest.find({}).then(quests => {
+        let quest = quests[0]
+        chai
+          .request(app)
+          .get("/")
+          .end((err, res) => {
+            expect(res.text).to.contain(quest.title)
+            expect(res.text).to.contain(quest.quest_giver)
+            // expect(res.text).to.contain(quest.rewards)
+            // expect(res.text).to.contain(quest.locations)
+            done()
+          })
+      })
+    })
+  })
+
+  describe("GET show view for Quest ('/quest/:id')", () => {
+    it("should return a successful response 200", done => {
+      Quest.findOne({}).then(quest => {
+        console.log(quest)
+        chai
+          .request(app)
+          .get(`/quest/${quest._id}`)
+          .end((err, res) => {
+            expect(res).to.have.status(200)
+            done()
+          })
+      })
+    })
+
+    it("should include the fields of a Quest", done => {
+      Quest.findOne({}).then(quest => {
+        chai
+          .request(app)
+          .get(`/quest/${quest._id}`)
+          .end((err, res) => {
+            expect(res.text).to.contain(quest.quest_giver)
+            expect(res.text).to.contain(quest.title)
+            expect(res.text).to.contain(quest.rewards)
+            expect(res.text).to.contain(quest.locations)
+            done()
+          })
+      })
+    })
+  })
+})
